3dedb544e9faba57b59a5f29cd4176ef50cd689e
Harrison <harrison@dev.com>
2025-11-28 14:11:56 -0500

    Initial commit

---
+++ src/main/java/com/estudo/ajudame/AjudameApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.ajudame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjudameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjudameApplication.class, args);
+	}
+
+}
